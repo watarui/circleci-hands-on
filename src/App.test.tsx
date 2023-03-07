b76712f4e1ab8813924866e3969b3ad505405fb3
Wataru Ishikawa <wataru-ishikawa@vcube.co.jp>
2023-03-07 20:02:19 +0900

    green

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render, screen, fireEvent} from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-describe('Click button', () => {
+describe("Click button", () => {
   render(<App />);
   fireEvent.click(screen.getByTestId("Button"));
 
-  test('Text should be shown', () => {
-    expect(screen.getByTestId('Text')).toHaveTextContent('CircleCI');
+  test("Text should be shown", () => {
+    expect(screen.getByTestId("Text")).toHaveTextContent("Hello");
   });
 });
